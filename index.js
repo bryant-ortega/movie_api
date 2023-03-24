17ef41f7d17eb881365a42dd244ecebcb673051e
Bryant Ortega <kalicorose@gmail.com>
2023-03-24 12:24:03 -0700

    appending Morgan logs added to index.js

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require("express"),
-    morgan = require("morgan");
+    morgan = require("morgan"),
+    fs = require("fs"),
+    path = require("path");
 
 const app = express();
+// create a write stream (in append mode)
+// a ‘log.txt’ file is created in root directory
+const accessLogStream = fs.createWriteStream(path.join(__dirname, "log.txt"), {
+    flags: "a",
+});
 
-app.use(morgan("common"));
+app.use(morgan("combined", { stream: accessLogStream }));
 
 app.get("/", (req, res) => {
     res.send("Welcome to my app!");
