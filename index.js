e2f390f3bb1594db7c9e3112dfae2dd66e5d97b3
Bryant Ortega <kalicorose@gmail.com>
2023-04-16 15:06:09 -0700

    added auth to /movies in index.js

--- index.js
+++ index.js
@@ -33,16 +33,20 @@ const passport = require("passport");
 require("./passport");
 
 // MONGOOSE Get all movies
-app.get("/movies", (req, res) => {
-    Movies.find()
-        .then(movies => {
-            res.status(201).json(movies);
-        })
-        .catch(err => {
-            console.error(err);
-            res.status(500).send("Error: " + err);
-        });
-});
+app.get(
+    "/movies",
+    passport.authenticate("jwt", { session: false }),
+    (req, res) => {
+        Movies.find()
+            .then(movies => {
+                res.status(201).json(movies);
+            })
+            .catch(err => {
+                console.error(err);
+                res.status(500).send("Error: " + err);
+            });
+    }
+);
 
 // MONGOOSE Get movie by title
 app.get("/movies/:Title", (req, res) => {
