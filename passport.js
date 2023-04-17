40c0fbbff155b3d68d71b1deeedde58d7f33996d
Bryant Ortega <kalicorose@gmail.com>
2023-04-17 14:36:53 -0700

    updated passport.js

--- passport.js
+++ passport.js
@@ -22,9 +22,20 @@ passport.use(
                         message: "Incorrect username or password.",
                     })
                 );
-        }
-    )
-);
+
+         if (!user) {
+            console.log('incorrect username');
+            return callback(null, false, {message: 'Incorrect username.'});
+    }
+
+    if (!user.validatePassword(password)) {
+      console.log('incorrect password');
+      return callback(null, false, {message: 'Incorrect password.'});
+    }
+
+    console.log('finished');
+    return callback(null, user);
+}));
 
 passport.use(
     new JWTStrategy(
