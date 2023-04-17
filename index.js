d799652fdc46b5ce245512ec477565937f7b8b7c
Bryant Ortega <kalicorose@gmail.com>
2023-04-16 19:20:49 -0700

    updated endpoints w auth in index.js

--- index.js
+++ index.js
@@ -49,94 +49,118 @@ app.get(
 );
 
 // MONGOOSE Get movie by title
-app.get("/movies/:Title", (req, res) => {
-    Movies.findOne({ Title: req.params.Title })
-        .then(movie => {
-            res.json(movie);
-        })
-        .catch(err => {
-            console.error(err);
-            res.status(500).send("Error: " + err);
-        });
-});
+app.get(
+    "/movies/:Title",
+    passport.authenticate("jwt", { session: false }),
+    (req, res) => {
+        Movies.findOne({ Title: req.params.Title })
+            .then(movie => {
+                res.json(movie);
+            })
+            .catch(err => {
+                console.error(err);
+                res.status(500).send("Error: " + err);
+            });
+    }
+);
 
 // MONGOOSE Get Genre By Name
-app.get("/movies/genre/:Name", (req, res) => {
-    Movies.findOne({ "Genre.Name": req.params.Name })
-        .then(movie => {
-            res.json(movie.Genre);
-        })
-        .catch(err => {
-            console.error(err);
-            res.status(500).send("Error: " + err);
-        });
-});
+app.get(
+    "/movies/genre/:Name",
+    passport.authenticate("jwt", { session: false }),
+    (req, res) => {
+        Movies.findOne({ "Genre.Name": req.params.Name })
+            .then(movie => {
+                res.json(movie.Genre);
+            })
+            .catch(err => {
+                console.error(err);
+                res.status(500).send("Error: " + err);
+            });
+    }
+);
 
 //MONGOOSE Get Director Info By Name
-app.get("/movies/director/:Name", (req, res) => {
-    Movies.findOne({ "Director.Name": req.params.Name })
-        .then(movie => {
-            res.json(movie.Director);
-        })
-        .catch(err => {
-            console.error(err);
-            res.status(500).send("Error: " + err);
-        });
-});
+app.get(
+    "/movies/director/:Name",
+    passport.authenticate("jwt", { session: false }),
+    (req, res) => {
+        Movies.findOne({ "Director.Name": req.params.Name })
+            .then(movie => {
+                res.json(movie.Director);
+            })
+            .catch(err => {
+                console.error(err);
+                res.status(500).send("Error: " + err);
+            });
+    }
+);
 
 // MONGOOSE Add a user
-app.post("/users", (req, res) => {
-    Users.findOne({ Username: req.body.Username })
-        .then(user => {
-            if (user) {
-                return res
-                    .status(400)
-                    .send(req.body.Username + "already exists");
-            } else {
-                Users.create({
-                    Username: req.body.Username,
-                    Password: req.body.Password,
-                    Email: req.body.Email,
-                    Birthday: req.body.Birthday,
-                })
-                    .then(user => {
-                        res.status(201).json(user);
+app.post(
+    "/users",
+    passport.authenticate("jwt", { session: false }),
+    (req, res) => {
+        Users.findOne({ Username: req.body.Username })
+            .then(user => {
+                if (user) {
+                    return res
+                        .status(400)
+                        .send(req.body.Username + "already exists");
+                } else {
+                    Users.create({
+                        Username: req.body.Username,
+                        Password: req.body.Password,
+                        Email: req.body.Email,
+                        Birthday: req.body.Birthday,
                     })
-                    .catch(error => {
-                        console.error(error);
-                        res.status(500).send("Error: " + error);
-                    });
-            }
-        })
-        .catch(error => {
-            console.error(error);
-            res.status(500).send("Error: " + error);
-        });
-});
+                        .then(user => {
+                            res.status(201).json(user);
+                        })
+                        .catch(error => {
+                            console.error(error);
+                            res.status(500).send("Error: " + error);
+                        });
+                }
+            })
+            .catch(error => {
+                console.error(error);
+                res.status(500).send("Error: " + error);
+            });
+    }
+);
 
 // MONGOOSE Get all users
-app.get("/users", (req, res) => {
-    Users.find()
-        .then(users => {
-            res.status(201).json(users);
-        })
-        .catch(err => {
-            console.error(err);
-            res.status(500).send("Error: " + err);
-        });
-});
+app.get(
+    "/users",
+    passport.authenticate("jwt", { session: false }),
+    (req, res) => {
+        Users.find()
+            .then(users => {
+                res.status(201).json(users);
+            })
+            .catch(err => {
+                console.error(err);
+                res.status(500).send("Error: " + err);
+            });
+    }
+);
 
 // MONGOOSE Get a user by username
-app.get("/users/:Username", (req, res) => {
-    Users.findOne({ Username: req.params.Username })
-        .then(user => {
-            res.json(user);
-        })
-        .catch(err => {
-            console.error(err);
-            res.status(500).send("Error: " + err);
-        });
-});
+app.get(
+    "/users/:Username",
+    passport.authenticate("jwt", { session: false }),
+    (req, res) => {
+        Users.findOne({ Username: req.params.Username })
+            .then(user => {
+                res.json(user);
+            })
+            .catch(err => {
+                console.error(err);
+                res.status(500).send("Error: " + err);
+            });
+    }
+);
 
 // MONGOOSE Update a user's info, by username
 /* We’ll expect JSON in this format
@@ -149,96 +173,124 @@ app.get("/users/:Username", (req, res) => {
   (required)
   Birthday: Date
 }*/
-app.put("/users/:Username", (req, res) => {
-    Users.findOneAndUpdate(
-        { Username: req.params.Username },
-        {
-            $set: {
-                Username: req.body.Username,
-                Password: req.body.Password,
-                Email: req.body.Email,
-                Birthday: req.body.Birthday,
+app.put(
+    "/users/:Username",
+    passport.authenticate("jwt", { session: false }),
+    (req, res) => {
+        Users.findOneAndUpdate(
+            { Username: req.params.Username },
+            {
+                $set: {
+                    Username: req.body.Username,
+                    Password: req.body.Password,
+                    Email: req.body.Email,
+                    Birthday: req.body.Birthday,
+                },
             },
-        },
-        { new: true } // This line makes sure that the updated document is returned
-    )
-        .then(user => {
-            if (!user) {
-                return res.status(400).send(req.params.Username + "not found");
-            }
-            res.json(user);
-        })
-        .catch(err => {
-            console.error(err);
-            res.status(500).send("Error: " + err);
-        });
-});
+            { new: true } // This line makes sure that the updated document is returned
+        )
+            .then(user => {
+                if (!user) {
+                    return res
+                        .status(400)
+                        .send(req.params.Username + "not found");
+                }
+                res.json(user);
+            })
+            .catch(err => {
+                console.error(err);
+                res.status(500).send("Error: " + err);
+            });
+    }
+);
 
 // MONGOOSE Add a movie to a user's list of favorites
-app.post("/users/:Username/movies/:MovieID", (req, res) => {
-    Users.findOneAndUpdate(
-        { Username: req.params.Username },
-        {
-            $push: { FavoriteMovies: req.params.MovieID },
-        },
-        { new: true } // This line makes sure that the updated document is returned
-    )
-        .then(user => {
-            if (!user) {
-                return res.status(400).send(req.params.Username + "not found");
-            }
-            res.json(user);
-        })
-        .catch(err => {
-            console.error(err);
-            res.status(500).send("Error: " + err);
-        });
-});
+app.post(
+    "/users/:Username/movies/:MovieID",
+    passport.authenticate("jwt", { session: false }),
+    (req, res) => {
+        Users.findOneAndUpdate(
+            { Username: req.params.Username },
+            {
+                $push: { FavoriteMovies: req.params.MovieID },
+            },
+            { new: true } // This line makes sure that the updated document is returned
+        )
+            .then(user => {
+                if (!user) {
+                    return res
+                        .status(400)
+                        .send(req.params.Username + "not found");
+                }
+                res.json(user);
+            })
+            .catch(err => {
+                console.error(err);
+                res.status(500).send("Error: " + err);
+            });
+    }
+);
 
 //MONGOOSE Delete movie from list of favorites
-app.delete("/users/:Username/movies/:MovieID", (req, res) => {
-    Users.findOneAndUpdate(
-        { Username: req.params.Username },
-        {
-            $pull: { FavoriteMovies: req.params.MovieID },
-        },
-        { new: true } // This line makes sure that the updated document is returned
-    )
-        .then(user => {
-            if (!user) {
-                return res.status(400).send(req.params.Username + "not found");
-            }
-            res.json(user);
-        })
-        .catch(err => {
-            console.error(err);
-            res.status(500).send("Error: " + err);
-        });
-});
+app.delete(
+    "/users/:Username/movies/:MovieID",
+    passport.authenticate("jwt", { session: false }),
+    (req, res) => {
+        Users.findOneAndUpdate(
+            { Username: req.params.Username },
+            {
+                $pull: { FavoriteMovies: req.params.MovieID },
+            },
+            { new: true } // This line makes sure that the updated document is returned
+        )
+            .then(user => {
+                if (!user) {
+                    return res
+                        .status(400)
+                        .send(req.params.Username + "not found");
+                }
+                res.json(user);
+            })
+            .catch(err => {
+                console.error(err);
+                res.status(500).send("Error: " + err);
+            });
+    }
+);
 
 //MONGOOSE DELETE user
-app.delete("/users/:Username", (req, res) => {
-    Users.findOneAndRemove({ Username: req.params.Username })
-        .then(user => {
-            if (!user) {
-                res.status(400).send(req.params.Username + " was not found");
-            } else {
-                res.status(200).send(req.params.Username + " was deleted.");
-            }
-        })
-        .catch(err => {
-            console.error(err);
-            res.status(500).send("Error: " + err);
-        });
-});
+app.delete(
+    "/users/:Username",
+    passport.authenticate("jwt", { session: false }),
+    (req, res) => {
+        Users.findOneAndRemove({ Username: req.params.Username })
+            .then(user => {
+                if (!user) {
+                    res.status(400).send(
+                        req.params.Username + " was not found"
+                    );
+                } else {
+                    res.status(200).send(req.params.Username + " was deleted.");
+                }
+            })
+            .catch(err => {
+                console.error(err);
+                res.status(500).send("Error: " + err);
+            });
+    }
+);
 
-app.get("/", (req, res) => {
+app.get("/", passport.authenticate("jwt", { session: false }), (req, res) => {
     res.send("Welcome to myflix!");
 });
 
-app.get("/secreturl", (req, res) => {
-    res.send("This is a secret url with super top-secret content.");
-});
+app.get(
+    "/secreturl",
+    passport.authenticate("jwt", { session: false }),
+    (req, res) => {
+        res.send("This is a secret url with super top-secret content.");
+    }
+);
 
 app.use((err, req, res, next) => {
     console.error(err.stack);
