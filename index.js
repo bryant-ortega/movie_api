b0b62035e96764b14bddebdc3aedf6d1079ddc14
Bryant Ortega <kalicorose@gmail.com>
2023-04-19 13:20:51 -0700

    Heroku updates for package.json and index.js

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ const accessLogStream = fs.createWriteStream(path.join(__dirname, "log.txt"), {
     flags: "a",
 });
 
+const { check, validationResult } = require("express-validator");
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const cors = require("cors");
@@ -116,36 +118,59 @@ app.get(
 );
 
 // MONGOOSE Add a user
-app.post("/users", (req, res) => {
-    let hashedPassword = Users.hashPassword(req.body.Password);
-    Users.findOne({ Username: req.body.Username }) // Search to see if a user with the requested username already exists
-        .then(user => {
-            if (user) {
-                //If the user is found, send a response that it already exists
-                return res
-                    .status(400)
-                    .send(req.body.Username + " already exists");
-            } else {
-                Users.create({
-                    Username: req.body.Username,
-                    Password: hashedPassword,
-                    Email: req.body.Email,
-                    Birthday: req.body.Birthday,
-                })
-                    .then(user => {
-                        res.status(201).json(user);
+app.post(
+    "/users", // Validation logic here for request
+    //you can either use a chain of methods like .not().isEmpty()
+    //which means "opposite of isEmpty" in plain english "is not empty"
+    //or use .isLength({min: 5}) which means
+    //minimum value of 5 characters are only allowed
+    [
+        check("Username", "Username is required").isLength({ min: 5 }),
+        check(
+            "Username",
+            "Username contains non alphanumeric characters - not allowed."
+        ).isAlphanumeric(),
+        check("Password", "Password is required").not().isEmpty(),
+        check("Email", "Email does not appear to be valid").isEmail(),
+    ],
+    (req, res) => {
+        // check the validation object for errors
+        let errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            return res.status(422).json({ errors: errors.array() });
+        }
+
+        let hashedPassword = Users.hashPassword(req.body.Password);
+        Users.findOne({ Username: req.body.Username }) // Search to see if a user with the requested username already exists
+            .then(user => {
+                if (user) {
+                    //If the user is found, send a response that it already exists
+                    return res
+                        .status(400)
+                        .send(req.body.Username + " already exists");
+                } else {
+                    Users.create({
+                        Username: req.body.Username,
+                        Password: hashedPassword,
+                        Email: req.body.Email,
+                        Birthday: req.body.Birthday,
                     })
-                    .catch(error => {
-                        console.error(error);
-                        res.status(500).send("Error: " + error);
-                    });
-            }
-        })
-        .catch(error => {
-            console.error(error);
-            res.status(500).send("Error: " + error);
-        });
-});
+                        .then(user => {
+                            res.status(201).json(user);
+                        })
+                        .catch(error => {
+                            console.error(error);
+                            res.status(500).send("Error: " + error);
+                        });
+                }
+            })
+            .catch(error => {
+                console.error(error);
+                res.status(500).send("Error: " + error);
+            });
+    }
+);
 
 // MONGOOSE Get all users
 app.get(
@@ -314,6 +339,7 @@ app.use((err, req, res, next) => {
     res.status(500).send("Something broke!");
 });
 
-app.listen(8080, () => {
-    console.log("Your app is listening on port 8080.");
+const port = process.env.PORT || 8080;
+app.listen(port, "0.0.0.0", () => {
+    console.log("Listening on Port " + port);
 });
