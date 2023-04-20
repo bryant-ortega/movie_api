fdcf576083fb216b446911eb8d4fc188c6b6f82b
Bryant Ortega <kalicorose@gmail.com>
2023-04-19 18:54:59 -0700

    re-added to index.js

--- index.js
+++ index.js
@@ -50,7 +50,10 @@ const Movies = Models.Movie;
 const Users = Models.User;
 
 //mongoose.connect("mongodb://127.0.0.1:27017/cfDB", { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.connect(process.env.CONNECTION_URI);
+mongoose.connect(process.env.CONNECTION_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
 app.use(bodyParser.json());
 
