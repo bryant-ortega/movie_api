22ec9c62f6cdbf66026342accb01694b9034d4ec
Bryant Ortega <kalicorose@gmail.com>
2023-05-03 23:00:49 -0700

    deleted passport in index.js

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ app.get("/", (req, res) => {
 // MONGOOSE Get all movies
 app.get(
     "/movies",
-    passport.authenticate("jwt", { session: false }),
+
     (req, res) => {
         Movies.find()
             .then(movies => {
